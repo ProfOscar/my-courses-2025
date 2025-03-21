0de8ce5a083b0fe23f573087095a58b74c1ce57f
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-03-21 13:23:50 +0100

    Iniziato creazione CourseViewModel specializzata per elenco corsi

--- MyCourses.Models/Services/CourseService.cs
+++ MyCourses.Models/Services/CourseService.cs
@@ -1,22 +1,24 @@
-﻿namespace MyCourses.Models.Services
+﻿using MyCourses.Models.ViewModels;
+
+namespace MyCourses.Models.Services
 {
     public class CourseService
     {
-        public List<Course> GetCourses()
+        public List<CourseViewModel> GetCourses()
         {
             // dovrà leggere i dati dal DB e restituire la lista dei corsi
             // provvisoriamente creiamo dati casuali di test in memoria
-            var courseList = new List<Course>();
+            var courseList = new List<CourseViewModel>();
             var rnd = new Random();
             for (int i = 1; i <= 20; i++)
             {
                 var price = Convert.ToDecimal(rnd.NextDouble() * 10 + 10);
-                var course = new Course(
-                        i,
-                        $"Corso {i}",
-                        $"Description {i}",
-                        "/logo.svg"
-                    );
+                var course = new CourseViewModel
+                {
+                    Id = i,
+                    Title = $"Corso {i}",
+                    ImagePath = "/logo.svg"
+                };
                 courseList.Add(course);
             }
             return courseList;
---
+++ MyCourses.Models/ViewModels/CourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCourses.Models.ViewModels
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public string Author { get; set; }
+        public double Rating { get; set; }
+        public Decimal FullPrice { get; set; }
+        public Decimal CurrentPrice { get; set; }
+    }
+}
