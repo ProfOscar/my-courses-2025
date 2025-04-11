0e9e3826fc356ec9c1a1dfa55b88889ba0043956
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-04-11 12:27:17 +0200

    Esternalizzato in appsettings la stringa di connessione e iniettato IConfiguration in AdoNetCourseService

--- MyCourses.Models/Services/AdoNetCourseService.cs
+++ MyCourses.Models/Services/AdoNetCourseService.cs
@@ -1,44 +1,46 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using MyCourses.Models.Enums;
 using MyCourses.Models.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyCourses.Models.Services
 {
     public class AdoNetCourseService : ICourseService
     {
+        private readonly string _connStr;
+
+        public AdoNetCourseService(IConfiguration config)
+        {
+            _connStr = config.GetConnectionString("DefaultConnection")!;
+        }
+
         public List<CourseViewModel> GetCourses()
         {
             var courseList = new List<CourseViewModel>();
-            string connStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Dati\\MyCourses.mdf;Integrated Security=True;Connect Timeout=30";
-            using (var conn = new SqlConnection(connStr))
+            using var conn = new SqlConnection(_connStr);
+            conn.Open();
+            string sql = "SELECT Id, Title, ImagePath, Author, Rating, FullPrice_Currency, FullPrice_Amount, CurrentPrice_Currency, CurrentPrice_Amount FROM Courses";
+            using var cmd = new SqlCommand(sql, conn);
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
             {
-                conn.Open();
-                string sql = "SELECT Id, Title, ImagePath, Author, Rating, FullPrice_Currency, FullPrice_Amount, CurrentPrice_Currency, CurrentPrice_Amount FROM Courses";
-                using (var cmd = new SqlCommand(sql, conn))
+                CourseViewModel course = new CourseViewModel()
                 {
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            CourseViewModel course = new CourseViewModel()
-                            {
-                                Id = reader.GetInt32(0),
-                                Title = reader.GetString(1),
-                                ImagePath = reader.GetString(2),
-                                Author = reader.GetString(3),
-                                Rating = reader.GetFloat(4),
-                                FullPrice = new Money(Enums.Currency.EUR, reader.GetDecimal(6)),
-                                CurrentPrice = new Money(Enums.Currency.EUR, reader.GetDecimal(8))
-                                //TODO: gestire correttamente la valuta
-                            };
-                            courseList.Add(course);
-                        }
-                    }
-                }
+                    Id = reader.GetInt32(0),
+                    Title = reader.GetString(1),
+                    ImagePath = reader.GetString(2),
+                    Author = reader.GetString(3),
+                    Rating = reader.GetFloat(4),
+                    FullPrice = new Money(
+                        Enum.Parse<Currency>(reader.GetString(5)),
+                        reader.GetDecimal(6)
+                    ),
+                    CurrentPrice = new Money(
+                        Enum.Parse<Currency>(reader.GetString(7)),
+                        reader.GetDecimal(8)
+                    )
+                };
+                courseList.Add(course);
             }
             return courseList;
         }
