0d62de6ac0c13a8e857f9b12b59818758fcc776f
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-05-09 12:44:30 +0200

    Realizzato dettaglio corso selezionato con relative lezioni

--- MyCourses.Models/Services/AdoNetCourseService.cs
+++ MyCourses.Models/Services/AdoNetCourseService.cs
@@ -47,7 +47,46 @@ namespace MyCourses.Models.Services
 
         public CourseDetailViewModel GetCourse(int id)
         {
-            throw new NotImplementedException();
+            using var conn = new SqlConnection(_connStr);
+            conn.Open();
+            string sql = "SELECT Id, Title, Description, ImagePath, Author, Rating, FullPrice_Currency, FullPrice_Amount, CurrentPrice_Currency, CurrentPrice_Amount FROM Courses WHERE Id=@Id;";
+            sql += "SELECT Id, Title, Description, Duration FROM Lessons WHERE CourseId=@Id";
+            using var cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.Add(new SqlParameter("Id", id));
+            using var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                CourseDetailViewModel course = new CourseDetailViewModel()
+                {
+                    Id = reader.GetInt32(0),
+                    Title = reader.GetString(1),
+                    Description = reader.GetString(2),
+                    ImagePath = reader.GetString(3),
+                    Author = reader.GetString(4),
+                    Rating = reader.GetFloat(5),
+                    FullPrice = new Money(
+                        Enum.Parse<Currency>(reader.GetString(6)),
+                        reader.GetDecimal(7)
+                    ),
+                    CurrentPrice = new Money(
+                        Enum.Parse<Currency>(reader.GetString(8)),
+                        reader.GetDecimal(9)
+                    )
+                };
+                reader.NextResult();
+                List<LessonViewModel> lessons = new List<LessonViewModel>();
+                while (reader.Read())
+                {
+                    lessons.Add(new LessonViewModel()
+                    {
+                        Title = Convert.ToString(reader["Title"]),
+                        Duration = TimeSpan.Parse(Convert.ToString(reader["Duration"]))
+                    });
+                }
+                course.Lessons = lessons;
+                return course;
+            }
+            throw new InvalidOperationException($"La query dovrebbe restituire i dati del corso {id}");
         }
     }
 }
